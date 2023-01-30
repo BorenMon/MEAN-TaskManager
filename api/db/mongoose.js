b161d725416d9d10204e59b2661dec9c81726b20
Mon Boren <63392246+BorenMon@users.noreply.github.com>
2023-01-30 22:04:52 +0700

    Update mongoose.js

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -4,13 +4,11 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 mongoose.set("strictQuery", false);
-// mongoose.connect('mongodb+srv://boren:Sbg7zMzFMSZOmOOa@task-manager.zanqn2a.mongodb.net/test', { useNewUrlParser: true }).then(() => {
-//   console.log('Connected to MongoDB')
-// }).catch(e => {
-//   console.log('Error while attempting to connect to MongoDB')
-//   console.log(e)
-// })
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager', { useNewUrlParser: true }).then(() => {
+mongoose.connect(
+  'mongodb+srv://boren:Sbg7zMzFMSZOmOOa@task-manager.zanqn2a.mongodb.net/test',
+  // 'mongodb://127.0.0.1:27017/task-manager',
+   { useNewUrlParser: true }
+).then(() => {
   console.log('Connected to MongoDB')
 }).catch(e => {
   console.log('Error while attempting to connect to MongoDB')
