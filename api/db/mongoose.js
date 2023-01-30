52a904924baff9ddb5adfdf7d93a3054644620e6
Mon Boren <63392246+BorenMon@users.noreply.github.com>
2023-01-30 21:03:25 +0700

    *

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,6 +3,13 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
+mongoose.set("strictQuery", false);
+// mongoose.connect('mongodb+srv://boren:qOsLCbJpKqPOWBR8@task-manager.v5ctskh.mongodb.net/test', { useNewUrlParser: true }).then(() => {
+//   console.log('Connected to MongoDB')
+// }).catch(e => {
+//   console.log('Error while attempting to connect to MongoDB')
+//   console.log(e)
+// })
 mongoose.connect('mongodb://localhost:27017/task-manager', { useNewUrlParser: true }).then(() => {
   console.log('Connected to MongoDB')
 }).catch(e => {
